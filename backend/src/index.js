bc51a0e96cf9ea41f1ddda591b715c5979bd9c35
unknown <kavinkumar6482@gmail.com>
2025-09-02 13:02:45 +0530

    first commit

--- backend/src/index.js
+++ backend/src/index.js
@@ -14,6 +14,7 @@ import { app, server } from "./lib/socket.js";
 dotenv.config();
 
 const PORT = process.env.PORT;
+// console.log(PORT, "ederp");
 const __dirname = path.resolve();
 
 app.use(express.json());
